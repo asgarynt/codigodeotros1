7635ba63eba0201e7d539cd0e77b6df441f88604
AsgaryNT <asgarynt@gmail.com>
2023-03-22 00:18:25 -0600

    Ejercicio Codigo de otros 1 concluido

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,26 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+let baseEndpoint = 'https://api.github.com';
+let usersEndpoint = `${baseEndpoint}/users`;
+const n = document.querySelector('#name'); //El selector es de tipo id, por lo que debe ser #name y se elimina el $ ya que no es una buena practica usarlo al definir el nombre de variables.  
+const b = document.querySelector('#blog'); //El selector es de tipo id, por lo que debe ser #blog y se elimina el $ ya que no es una buena practica usarlo al definir el nombre de variables.
+const l = document.querySelector('#location');//El selector es de tipo id, por lo que debe ser #location y se elimina el $ ya que no es una buena practica usarlo al definir el nombre de variables.
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+
+async function displayUser(username) { //Se agrega async antes de la funcion para que la funcion devuelva una promesa
+  n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await response.json(); // Se agrega la promesa que permite convertir el contenido obtenido por fetch en un JSON y acceder por medio de la constante data a los atributos
+  console.log(data); 
+  
+  n.textContent = `${data.name}`; //Se utilizan batckticks para que funcione la plantilla literal
+  b.textContent = `${data.blog}`; //Se utilizan batckticks para que funcione la plantilla literal
+  l.textContent = `${data.location}`; //Se utilizan batckticks para que funcione la plantilla literal
 }
+
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
